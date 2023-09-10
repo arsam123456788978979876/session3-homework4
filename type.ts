53a43f0db43e5d58de2025c7152ff6f36486bbc7
arsam <arsamsarkhosh4@gmail.com>
2023-09-11 00:56:21 +0330

    h

---
+++ type.ts
@@ -0,0 +1,77 @@
+// studants grade 
+
+const grade: number = 100
+const number2: number = 85
+const number3: number = 75
+const number4: number = 65
+const number5: number = 55
+const number6: number = 45
+
+if(grade > 90 ) {
+    console.log("a")
+}
+if(number2 > 80) {
+    console.log("b")
+} 
+if(number3 > 70) {
+    console.log("c")
+}
+if(number4 > 60) {
+    console.log("d")
+}
+if(number5 > 50) {
+    console.log("e")
+}
+if(number6 < 50) {
+    console.log("f")
+}
+
+// This part belongs to the traffic light
+
+let red = "red"
+let yellow = "yellow"
+let green = "green"
+
+ if(red == "red") {
+    console.log("yellow")
+ } if(yellow == "yellow") {
+    console.log("green")
+ } if (green == "green"){
+    console.log("red")
+ }
+
+var saturday: number = 0 
+var sunday: number = 1
+var monday: number = 2
+var tuesday: number = 3
+var wensday: number = 4
+var thursday: number = 5
+var friday: number = 6
+
+
+
+// when i wanna run my code, words become pell-mell because I wrote in persian
+
+if( saturday == 0 ){
+   console.log("شنبه")
+}
+if( sunday == 1 ){
+   console.log('یکشنبه')
+}
+if( monday == 2 ){
+   console.log("دوشنبه")
+}
+if( tuesday == 3 ){
+   console.log("سهشنبه")
+}
+if( wensday == 4 ){
+   console.log("چهارشنبه")
+}
+if( thursday == 5 ){
+   console.log("پنجشنبه")
+}
+if( friday ==6 ){
+   console.log('جمعه')
+}
+
+//                                                                      END                                                                             //
